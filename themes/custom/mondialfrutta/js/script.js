0c6d13ca366b402569ca13090da34484e3c0291b
Nicola De Lazzari <nicola.delazzari@gmail.com>
2017-01-03 16:05:18 +0100

    added new row and new menu hamburger

--- themes/custom/mondialfrutta/js/script.js
+++ themes/custom/mondialfrutta/js/script.js
@@ -1,41 +1,53 @@
-$('.js-hamburger').on('click', function() {
-    $('.js-menu-container').toggleClass('open');
-    $('.overlay').toggleClass('open');
-});
+(function ($) {
+    "use strict";
 
-// Get a reference to the <path>
-var path = document.querySelector('#path');
+    $('.js-hamburger').on('click', function() {
+        $('.js-menu-container, .js-hamburger').toggleClass('open');
+        $('.overlay').toggleClass('open');
+    });
 
-// Get length of path
-var pathLength = path.getTotalLength();
+    $(window).scroll(function() {
+        if ( $(window).scrollTop() > $('.visual').height() ) {
+            $('header').addClass('open');
+        } else {
+            $('header').removeClass('open')
+        }
+    });
 
-$('.path').css('top', $('.visual').height());
+    // Get a reference to the <path>
+    var path = document.querySelector('#path');
 
-// Make very long dashes (the length of the path itself)
-path.style.strokeDasharray = pathLength + ' ' + pathLength;
+    // Get length of path
+    var pathLength = path.getTotalLength();
 
-// Offset the dashes so the it appears hidden entirely
-path.style.strokeDashoffset = pathLength;
+    $('.path').css('top', $('.visual').height());
 
-// When the page scrolls...
-window.addEventListener("scroll", function(e) {
-
-  // What % down is it?
-  var scrollPercentage = ( (document.documentElement.scrollTop + document.body.scrollTop) - $('.visual').height() ) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
-
-  // Length to offset the dashes
-  var drawLength = pathLength * (scrollPercentage * 1.15);
-
-  // Draw in reverse
-  path.style.strokeDashoffset = pathLength - drawLength;
-
-  // ... at bottom of scrolling function
-
-  // When complete, remove the dash array, otherwise shape isn't quite sharp
-  if (scrollPercentage >= 0.99) {
-    path.style.strokeDasharray = "none";
-  } else {
+    // Make very long dashes (the length of the path itself)
     path.style.strokeDasharray = pathLength + ' ' + pathLength;
-  }
 
-});
+    // Offset the dashes so the it appears hidden entirely
+    path.style.strokeDashoffset = pathLength;
+
+    // When the page scrolls...
+    window.addEventListener("scroll", function(e) {
+
+      // What % down is it?
+      var scrollPercentage = ( (document.documentElement.scrollTop + document.body.scrollTop) - $('.visual').height() ) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+
+      // Length to offset the dashes
+      var drawLength = pathLength * (scrollPercentage * 1.5);
+
+      // Draw in reverse
+      path.style.strokeDashoffset = pathLength - drawLength;
+
+      // ... at bottom of scrolling function
+
+      // When complete, remove the dash array, otherwise shape isn't quite sharp
+      if (scrollPercentage >= 0.99) {
+        path.style.strokeDasharray = "none";
+      } else {
+        path.style.strokeDasharray = pathLength + ' ' + pathLength;
+      }
+
+    });
+})(jQuery);
