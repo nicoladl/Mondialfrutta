a4cb4d4d3c4c90b880b2649809aa91867cd64dc5
Nicola De Lazzari <nicola.delazzari@gmail.com>
2017-06-02 16:06:02 +0200

    added scripts, vars and makefile

---
+++ scripts/script.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Import/set all the variables.
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DOCROOT=${SCRIPT_DIR}/../docroot
+source ${SCRIPT_DIR}/variables.sh
+
+# Install the site.
+cd ${DOCROOT} && drush si ${PROFILE_NAME} -y --account-mail=${ACCOUNT_MAIL} --site-mail=${ACCOUNT_MAIL} --account-name=${ACCOUNT_NAME} --account-pass=${ACCOUNT_PASS}
+cd ${DOCROOT} && drush cset -y system.site uuid ${SITE_ID}
+
+# Import the configuration from configuration manager.
+cd ${DOCROOT} && drush cim sync -y
+
+cd ${DOCROOT} && drush mi --all
---
+++ scripts/variables.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+ACCOUNT_NAME="Nicola"
+ACCOUNT_PASS="Ronaldo10!"
+ACCOUNT_MAIL="nicola.delazzari@gmail.com"
+DRUPAL_CONTAINER_NAME="mondialfrutta"
+PROFILE_NAME="mondialfrutta"
+SITE_ID="local"
+PHP_PATH=$(which php)
+PHPUNIT_PATH=$(which phpunit)
