676d574f2f6e5c15afdc12b4c99a0d1e76d0be8d
Nicola De Lazzari <nicola.delazzari@gmail.com>
2017-06-02 12:59:42 +0200

    reorder client assets

---
+++ client/gulpfile.js
@@ -0,0 +1,145 @@
+/*==========================================================
+Mondialfrutta Gulpfile.js
+Author: Nicola De Lazzari
+==========================================================*/
+
+
+/**
+* List Gulp dependencies
+**/
+var gulp        = require('gulp'),
+    sass        = require('gulp-sass'),
+    prefix      = require('gulp-autoprefixer'),
+    plumber     = require('gulp-plumber'),
+    notify      = require("gulp-notify"),
+    del         = require('del'),
+    svgmin      = require('gulp-svgmin'),
+    svgstore    = require('gulp-svgstore'),
+    cheerio     = require('gulp-cheerio'),
+    rename      = require('gulp-rename'),
+    browserSync = require('browser-sync').create();
+
+
+/**
+*
+* Styles
+* - Catch errors (gulp-plumber)
+* - Autoprefixer
+* - Copy in theme folder
+*
+**/
+gulp.task('styles', function() {
+    gulp.src('scss/**/*.scss')
+        .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+        .pipe(sass())
+        .pipe(prefix())
+        .pipe(gulp.dest('../docroot/themes/custom/etinerary_theme/assets/css'))
+        .pipe(browserSync.stream());
+});
+
+
+/**
+*
+* Scripts
+* - Catch errors (gulp-plumber)
+* - Copy in theme folder
+*
+**/
+gulp.task('scripts', function() {
+    gulp.src('js/**/*.js')
+        .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+        .pipe(gulp.dest('../docroot/themes/custom/etinerary_theme/assets/js'))
+        .pipe(browserSync.stream());
+});
+
+/**
+ *
+ * Fonts
+ * - Copy in theme folder
+ *
+ **/
+gulp.task('fonts', function() {
+    gulp.src('fonts/*')
+        .pipe(gulp.dest('../docroot/themes/custom/etinerary_theme/assets/fonts'));
+});
+
+
+/**
+*
+* Images
+* - Copy images in web
+*
+**/
+gulp.task('images', function () {
+    del.sync(['../docroot/themes/custom/etinerary_theme/assets/img/**'], {force: true});
+    gulp.src('img/**/*.*')
+        .pipe(gulp.dest('../docroot/themes/custom/etinerary_theme/assets/img'))
+});
+
+
+/**
+ * Gulp Task
+ * =========
+ * Generate SVG Sprite
+ */
+
+gulp.task('svgstore', function() {
+    return gulp
+        .src( 'svg-sprite/**/*.svg' )
+        .pipe(rename({prefix: 'svg-'}))
+        .pipe(cheerio({
+            run: function ($) {
+                $('[fill]').removeAttr('fill');
+            },
+            parserOptions: { xmlMode: true }
+        }))
+        .pipe(svgstore({
+            inlineSvg: true
+        }))
+        .pipe(rename('_svgsprite.html.twig'))
+        .pipe(gulp.dest('../docroot/themes/custom/etinerary_theme/assets/svg-sprite'))
+});
+
+
+
+/**
+* Watch assets
+**/
+gulp.task('watch', function() {
+    gulp.watch('scss/**/*.scss', ['styles']);
+    gulp.watch('js/**/*.js', ['scripts']);
+    gulp.watch('img/**/*.*', ['images']);
+});
+
+
+/**
+*
+* Build task
+*
+**/
+gulp.task('build', function() {
+    gulp.start('styles', 'scripts', 'images', 'fonts', 'svgstore');
+});
+
+
+/**
+*
+* Serve - BrowserSync.io
+* - Watch CSS, JS, IMG & HTML for changes
+* - View project at: localhost:3000
+*
+**/
+gulp.task('serve', ['build','watch'], function() {
+    browserSync.init({
+        proxy: "local.etinerary.com"
+    });
+});
+
+
+/**
+*
+* Default task
+* - launch serve
+*
+**/
+gulp.task('default', ['serve']);
